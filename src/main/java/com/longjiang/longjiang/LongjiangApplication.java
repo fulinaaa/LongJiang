d1d94e7318541f6607d2bac77419bb5df09528a6
xyx <313136735@qq.com>
2024-03-06 17:08:42 +0800

    梦开始的一天

---
+++ src/main/java/com/longjiang/longjiang/LongjiangApplication.java
@@ -0,0 +1,13 @@
+package com.longjiang.longjiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongjiangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongjiangApplication.class, args);
+	}
+
+}
